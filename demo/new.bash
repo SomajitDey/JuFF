464775924f828392a2edf9e7cce61fcb63a376c7
Somajit <somajit@users.sourceforge.net>
2021-02-05 20:07:17 +0530

    Got it!! Refresh until key-press

--- demo/new.bash
+++ demo/new.bash
@@ -1,8 +1,13 @@
+#Display text with real-life updates. The moment user presses any key 
+#even if that key is an arrow key, go back to static view.
 trap exit INT 
 unset REPLY
+tput civis
 while [ -z "${REPLY}" ]; do
 tput clear
-timeout 1.5 sed -n 5,10p text
-read -t 0.5 -n 1
+timeout 0.5 tail -f text
+read -st 0.5 -n 1
 done
-echo done
+tput clear
+sed -n p text
+tput cnorm
