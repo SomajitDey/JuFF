04b8f0eca0cf7dce4cdfb10cdcceb01c06b3950b
Somajit <dey.somajit@gmail.com>
2021-02-22 15:55:58 +0530

    Now can send file by entering relative path as well

--- juff.bash
+++ juff.bash
@@ -152,6 +152,7 @@ declare -g SELFKEYID
 declare -rg REGISTRAR='registration#juff@github.com'
 declare -rg gpg="gpg"   #Ideally this should be the path to gpg or gpg2 + options
 declare -rg UIUPDATE="$(mktemp /tmp/uiupdate.XXXXXXXX)" #Flag file for communication between parallel processes (bg & fg)
+declare -rg ORIG_WD="${PWD}"
 }
 
 whiteline() {
@@ -637,14 +638,17 @@ display() {
             if [[ "${PAGE}" == '1' ]]; then
             cd ${TRUSTLOCAL}    #This is just so that bash autocompletes the typed user names on Tab press
             read -ep 'Input: ' INPUT    #Absence of -r so that backslash is escaped in \@gmail.com etc.
-            cd ${OLDPWD}
             else
+            cd "${ORIG_WD}" #So that users can choose files from their PWD by providing relative path
             read -erp 'Input: ' INPUT   #-r so that backslash is not escaped for Windows paths; C:\users etc.
             local TMP
-            [ "${INPUT:0:2}" == '~/' ] && TMP="${HOME}/${INPUT#*/}" && [ -f "${TMP}" ] && INPUT="${TMP}"
+            { [ "${INPUT:0:2}" == '~/' ] && TMP="${HOME}/${INPUT#*/}";} || \
+            { [ "${INPUT:0:1}" != '/' ] && TMP="${PWD}/${INPUT}";}
+            [ -f "${TMP}" ] && INPUT="${TMP}"
             TMP=$(wslpath "${INPUT}" 2>/dev/null) && [ -f "${TMP}" ] && INPUT="${TMP}"  #For WSL, transform Win path to Unix path
             unset TMP
             fi
+            cd ${OLDPWD}
             tput civis
         fi
 }
@@ -752,7 +756,7 @@ while [ -n "${PAGE}" ]; do
         fi
         [ -n "${CORRESPONDENT}" ] && backend ;;
     2)
-        frontend "${INBOX}/${CORRESPONDENT}.txt" 'Enter message or absolute path of the file to be sent [you may also drag & drop the file below]'
+        frontend "${INBOX}/${CORRESPONDENT}.txt" 'Enter message or path of the file to be sent [you may also drag & drop the file below]'
         case ${?} in 
         0)  MESSAGE="${INPUT}" ;;
         1 | 3 ) PAGE='1' ; PREV_CORR="${CORRESPONDENT}"
