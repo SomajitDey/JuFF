6e74dc482f28203100d2b53e59b2ab4d6a6b485f
Somajit <dey.somajit@gmail.com>
2021-03-12 00:33:57 +0530

    Alias is deprecated, source in non-interactive shell with 2>/dev/null

--- key_binding.bash
+++ key_binding.bash
@@ -1,12 +1,15 @@
-#!/usr/bin/env -S bash -i
+#!/usr/bin/env bash
+# Alternate shebang##!/usr/bin/env -S bash -i
 source_me(){
   for key in {a..z}; do
     echo bind -x \'\"${key}\":READLINE_LINE=\$\{READLINE_LINE:0:READLINE_POINT\}${key}\$\{READLINE_LINE:\$\(\(READLINE_POINT++\)\):\$\(\(\$\{#READLINE_LINE\}-\$\(\(READLINE_POINT-1\)\)\)\)\}\;echo hi\' >> source_me
   done
-  . ./source_me
-  rm ./source_me
-  alias read="read -er"
-} 2>/dev/null
+} 
+# Use of alias is deprecated, use shell function instead
+shopt -s expand_aliases
+alias go="read -er"
 source_me
-read
+. ./source_me 2>/dev/null
+go
 echo You typed: "$REPLY"
+rm ./source_me
